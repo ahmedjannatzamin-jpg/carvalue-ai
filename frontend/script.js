4b5046c7376d350249b07d050f5a784c948d80bb
ahmedjannatzamin-jpg <ahmedjannatzamin@gmail.com>
2026-09-08 11:58:45 +0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -19,7 +19,7 @@ form.addEventListener("submit", async function (e) {
     };
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://carvalue-ai-production.up.railway.app/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
